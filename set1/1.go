08872253e982ed8ca0b3fe7ea83f409674392fbc
dpl <tuchalia@gmail.com>
2015-10-13 15:30:13 +0200

    Add correct code up to the second challenge.

--- set1/1.go
+++ set1/1.go
@@ -17,28 +17,27 @@
 package main
 
 import (
-	"mcccrypt"
+	"bytes"
 	"fmt"
-	"strings"
+	mcc "github.com/dplbsd/mcclib"
 )
 
 // Symbols used in base64.
-
-var test_ascii = "I'm killing your brain like a poisonous mushroom"
-var test = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-var result = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+const test_ascii = "I'm killing your brain like a poisonous mushroom"
+const test = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+const result = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
 
 func main() {
 	fmt.Printf("Test string: %q len(test): %d\n", test, len(test))
 	fmt.Printf("Correct result: %q len(result): %d\n", result, len(result))
 
-	base64 := mcccrypt.Hex2base64([]byte(test))
+	base64 := mcc.Hex2base64(mcc.String2Hex(test))
 	fmt.Printf("Our result: %s\n", base64)
 
-	if cmp := strings.Compare(result, base64); cmp != 0 {
-		fmt.Printf("Your code is wrong!")
+	if cmp := bytes.Compare([]byte(result), base64); cmp != 0 {
+		fmt.Println("Your code is wrong!")
 	} else {
-		fmt.Printf("Your code is correct! :)")
+		fmt.Println("Your code is correct! :)")
 	}
 	return
 }
--- set1/2.go
+++ set1/2.go
@@ -1,6 +1,7 @@
 /*
  * Fixed XOR
- * Write a function that takes two equal-length buffers and produces their XOR combination.
+ * Write a function that takes two equal-length buffers and produces their XOR
+ * combination.
  *
  * If your function works properly, then when you feed it the string:
  *
@@ -16,71 +17,31 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"strings"
-	"strconv"
+	mcc "github.com/dplbsd/mcclib"
 )
 
-var first = "1c0111001f010100061a024b53535009181c"
-var second = "686974207468652062756c6c277320657965"
-var result = "746865206b696420646f6e277420706c6179"
-
-// Perform XOR for two arrays.
-func arrayxor(f []byte, s []byte) []byte {
-	fmt.Printf("f: %x\ns: %x\n", f, s)
-	l := len(f)
-	if l != len(s) {
-		return nil
-	}
-
-	ret := make([]byte, l, l)
-	for i := range ret {
-		fmt.Printf("f[%d]: %x, s[%d]: %x, ^: %x\n", i, byte(f[i]), i, byte(s[i]), f[i] ^ s[i])
-		ret[i] = byte(f[i]) ^ byte(s[i])
-	}
-	return ret
-}
-
-// Returns the hexadecimal values of a string with hexadecimal.
-func String2hex(s *[]byte) (ret *[]byte) {
-	ret := make([]byte, len(*s), cap(*s))
-	start := 0
-	if (*s)[0] == '0' && (*s[1] == 'x' || *s[1] == 'X') {
-		start = 2
-	}
-
-	// Appends byte if needed.
-	if len(*s) % 2 != 0 {
-		last := *s[len(s)-1]
-		*s = *s[:len(s)-1]
-		*s = append(s, '0', last)
-	}
-
-	for i := 0 ; i <= len(s); i+=2 {
-		strconv.ParseInt(*s[i:i+1], 16, 8)
-	}
-	return &ret
-}
+const first = "1c0111001f010100061a024b53535009181c"
+const second = "686974207468652062756c6c277320657965"
+const result = "746865206b696420646f6e277420706c6179"
 
 func main() {
-	for i,val := range first {
-		fmt.Printf("%x", first)
-	}
-	fmt.Printfln("")
-	for i,val := range String2hex(first) {
-		fmt.Printf("%x", val)
-	}
-	fmt.Printfln("")
-	fmt.Printf("First byte stream: %q\n", first)
-	fmt.Printf("Second byte stream: %q\n", second)
-	fmt.Printf("Correct result: %q\n", result)
-	xor := arrayxor([]byte(first), []byte(second))
-	fmt.Printf("Our result: %s\n", xor)
-	
-	if cmp := strings.Compare(result, string(xor)); cmp != 0 {
-		fmt.Printf("Your code is wrong!")
+	stringFirst := mcc.String2Hex(first)
+	stringSecond := mcc.String2Hex(second)
+	stringResult := mcc.String2Hex(result)
+	fmt.Printf("First byte stream: %q (%q)\n", first, stringFirst)
+	fmt.Printf("Second byte stream: %q (%q)\n", second, stringSecond)
+	fmt.Printf("Correct result: %q (%q)\n", result, stringResult)
+
+	xor := mcc.ArrayXor(mcc.String2Hex(first), mcc.String2Hex(second))
+	stringXor := mcc.Hex2String(xor)
+	fmt.Printf("\nOur result: %q (%q)\n", stringXor, xor)
+
+	if cmp := bytes.Compare(xor, mcc.String2Hex(result)); cmp != 0 {
+		fmt.Printf("Your code is wrong!\n")
 	} else {
-		fmt.Printf("Your code is correct! :)")
+		fmt.Printf("Your code is correct! :)\n")
 	}
 	return
 }
