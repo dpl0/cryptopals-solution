391483b3e876772b58483877d82c7020c1583314
dpl <tuchalia@gmail.com>
2015-10-15 23:48:35 +0200

    Implemented challenge 3.

--- mcclib/xor.go
+++ mcclib/xor.go
@@ -1,3 +1,26 @@
 package mcclib
 
 
+// Returns the XOR of two arrays of same length.
+func ArrayXor(f []byte, s []byte) []byte {
+	l := len(f)
+	if l != len(s) {
+		return nil
+	}
+	ret := make([]byte, l, l)
+	for i := range ret {
+		ret[i] = f[i] ^ s[i]
+	}
+	return ret
+}
+
+// Applies the XOR cipher to s.
+func KeyXor(k []byte, s []byte) []byte {
+	l := len(s)
+	lk := len(k)
+	ret := make([]byte, l, l)
+	for i := range ret {
+		ret[i] = s[i] ^ k[i%lk]
+	}
+	return ret
+}
---
+++ set1/3.go
@@ -0,0 +1,43 @@
+/*
+ * Single-byte XOR cipher
+ *
+ * The hex encoded string:
+ *
+ * 1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736
+ * ... has been XOR'd against a single character. Find the key, decrypt the
+ * message.
+ *
+ * You can do this by hand. But don't: write code to do it for you.
+ *
+ * How? Devise some method for "scoring" a piece of English plaintext.
+ * Character frequency is a good metric. Evaluate each output and choose the
+ * one with the best score.
+ *
+ * Achievement Unlocked
+ * You now have our permission to make "ETAOIN SHRDLU" jokes on Twitter.
+ */
+
+package main
+
+import (
+	"fmt"
+	mcc "github.com/dplbsd/mcclib"
+)
+
+const crypt = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+
+func main() {
+	hexCrypt :=  mcc.String2Hex(crypt)
+	// Key has to be of type []byte, even if just one byte.
+	key  := []byte{0}
+
+	for i := 0; i <= 255; i++ {
+		key[0] = byte(i)
+		decrypted := mcc.KeyXor(key, []byte(hexCrypt))
+		freq := mcc.SymbolFrequency(decrypted)
+		if mcc.IsDistSimilarTo(freq, mcc.EnglishDist) {
+			fmt.Printf("KEY FOUND: %s: %s\n", key, decrypted)
+		}
+	}
+	return
+}
