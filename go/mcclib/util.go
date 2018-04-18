f9b4538806701722a6003b0e8adafbbbc530b2ae
dpl <tuchalia@gmail.com>
2018-04-18 16:35:56 +0200

    Moved mcc utils to utils.go

--- go/mcclib/util.go
+++ go/mcclib/util.go
@@ -3,6 +3,7 @@ package mcclib
 import (
     "encoding/hex"
     "fmt"
+    "os"
 )
 
 // Convert a string representing hexa to actual hexa. Thanks encoding/hex!
@@ -22,4 +23,21 @@ func Hex2String(s []byte) string {
 func TestEqual(f []byte, s []byte) bool {
     // TODO
     return false
+}
+
+// * Define strings when working correctly, with colors
+const CorrectResult = "\x1b[6;30;42m" + "Your code is correct! :)\n" + "\x1b[0m"
+const WrongResult = "\x1b[6;30;41m" + "Your code is wrong! :(\n" + "\x1b[0m"
+
+func PrintCorrectly(str string) {
+    fmt.Printf("\x1b[6;30;42m"+str+"\x1b[0m\n");
+}
+func PrintWrongly(str string) {
+    fmt.Printf("\x1b[6;30;41m"+str+"\x1b[0m\n");
+}
+
+// * Define error handling function from Go (as in 2.go)
+func HandleError(err error) {
+    fmt.Fprintf(os.Stderr, "\nmcclib error: %s\n", err)
+    os.Exit(1)
 }
